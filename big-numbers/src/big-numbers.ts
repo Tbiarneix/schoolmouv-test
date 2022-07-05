26f1c771f3b016d72d808fd06c5398f37186dbb0
Thomas Biarneix <thomas.biarneix@voltyo.com>
2022-07-06 00:06:19 +0200

    Result without Type

--- big-numbers/src/big-numbers.ts
+++ big-numbers/src/big-numbers.ts
@@ -2,13 +2,23 @@ import { CustomOperation } from './custom-number.types'
 
 export type BigNumber = any
 
-const fromString = (s: string): BigNumber => undefined
+const fromString = (s: string): BigNumber => {
+  return parseInt(s, 10)
+}
 
-const add = (n1: BigNumber, n2: BigNumber): BigNumber => undefined
+const add = (n1: BigNumber, n2: BigNumber): BigNumber => {
+  const result = BigInt(n1) + BigInt(n2)
+  return `${result}`
+}
 
-const multiply = (n1: BigNumber, n2: BigNumber): BigNumber => undefined
+const multiply = (n1: BigNumber, n2: BigNumber): BigNumber => {
+  const result = BigInt(n1) * BigInt(n2)
+  return `${result}`
+}
 
-const toString = (n: BigNumber) => ''
+const toString = (n: BigNumber) => {
+  return n.toString()
+}
 
 export const BigNumberOperation: CustomOperation<BigNumber> = {
   fromString,
