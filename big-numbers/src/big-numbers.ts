d8ee454ac65f34385976a87f4c106da323162d14
Thomas Biarneix <thomas.biarneix@voltyo.com>
2022-07-06 00:13:54 +0200

    Types for functions and parameters

--- big-numbers/src/big-numbers.ts
+++ big-numbers/src/big-numbers.ts
@@ -2,21 +2,21 @@ import { CustomOperation } from './custom-number.types'
 
 export type BigNumber = any
 
-const fromString = (s: string): BigNumber => {
+const fromString = (s: string): number => {
   return parseInt(s, 10)
 }
 
-const add = (n1: BigNumber, n2: BigNumber): BigNumber => {
+const add = (n1: number, n2: number): string => {
   const result = BigInt(n1) + BigInt(n2)
   return `${result}`
 }
 
-const multiply = (n1: BigNumber, n2: BigNumber): BigNumber => {
+const multiply = (n1: number, n2: number): string => {
   const result = BigInt(n1) * BigInt(n2)
   return `${result}`
 }
 
-const toString = (n: BigNumber) => {
+const toString = (n: number): string => {
   return n.toString()
 }
 
