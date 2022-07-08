01bf4a2439919e9a2df70700ccda570d4ef21fb6
Thomas Biarneix <thomas.biarneix@voltyo.com>
2022-07-08 14:07:33 +0200

    Multiply + Type correction

--- big-numbers/src/big-numbers.ts
+++ big-numbers/src/big-numbers.ts
@@ -7,8 +7,8 @@ const fromString = (s: string): number => {
 }
 
 const add = (n1: number, n2: number): string => {
-  let s1 = ''
-  let s2 = ''
+  let s1: string = ''
+  let s2: string = ''
 
   if (n1.toString().length > n2.toString().length) {
     s1 = n1.toString()
@@ -18,11 +18,11 @@ const add = (n1: number, n2: number): string => {
     s2 = n1.toString()
   }
 
-  const result = []
-  let carry = 0
+  const result: string[] = []
+  let carry: number = 0
 
-  let a1 = Number(s1.charAt(s1.length - 1))
-  let a2 = Number(s2.charAt(s2.length - 1))
+  let a1: number = Number(s1.charAt(s1.length - 1))
+  let a2: number = Number(s2.charAt(s2.length - 1))
 
   for (let i = s1.length - 1; i >= 0; i--) {
     if (carry === 0) {
@@ -63,13 +63,64 @@ const add = (n1: number, n2: number): string => {
     }
   }
 
-  const stringResult = result.join('')
-  return stringResult
+  return result.join('')
 }
 
 const multiply = (n1: number, n2: number): string => {
-  const result = BigInt(n1) * BigInt(n2)
-  return `${result}`
+  let s1: string = ''
+  let s2: string = ''
+
+  if (n1.toString().length > n2.toString().length) {
+    s1 = n1.toString()
+    s2 = n2.toString()
+  } else {
+    s1 = n2.toString()
+    s2 = n1.toString()
+  }
+
+  const dividedN1: string[] = []
+  if (s1.length === 3) {
+    dividedN1.push(s1)
+  } else {
+    let cut: number = 0
+    let carry: number = s1.length
+    for (let i = s1.length - 1; i >= 0; i--) {
+      cut += 1
+      if (cut % 3 === 0) {
+        dividedN1.push(s1.slice(i, carry))
+        carry = i
+      }
+      if (i === 0) {
+        dividedN1.push(s1.slice(0, carry))
+      }
+    }
+  }
+
+  const correctedArrayResult: any[] = []
+  let addThousands: string = ''
+
+  dividedN1.forEach((e) => {
+    let addZero: string = ''
+    const arrayResult: string[] = []
+
+    for (let i = s2.length - 1; i > -1; i--) {
+      const result: number = parseInt(s2[i]) * parseInt(e)
+      arrayResult.push(result.toString())
+    }
+
+    for (let j = 0; j < arrayResult.length; j++) {
+      correctedArrayResult.push(`${arrayResult[j]}${addZero}${addThousands}`)
+      addZero += '0'
+    }
+    addThousands += '000'
+  })
+
+  const result: string = correctedArrayResult.reduce(
+    (previousValue, currentValue) => add(previousValue, currentValue),
+    0
+  )
+
+  return result
 }
 
 const toString = (n: number): string => {
